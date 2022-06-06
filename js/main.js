54bef56e39e55240ec42ca0eda302ac59431d143
rikizainal27 <rikiganteng2706@gmail.com>
2022-06-06 20:38:03 +0700

    update data

--- js/main.js
+++ js/main.js
@@ -1,27 +1,35 @@
-const nb = document.querySelector('.navigation');
-const icon = nb.querySelectorAll('.material-symbols-rounded');
-const ic = nb.querySelector('.material-symbols-rounded');
-const dark = document.querySelector('.dark');
-const style = document.querySelector('link');
-// dark mode
+const pertama = document.querySelector('.pertama');
+const kedua = document.querySelector('.kedua')
+const icon = document.querySelectorAll('.material-symbols-rounded');
+const ic = pertama.querySelector('.material-symbols-rounded');
+const ico = kedua.querySelector('.material-symbols-rounded');
+const dark = document.querySelector('.nav-2');
+const link = document.querySelector('link');
+const light = document.querySelector('.light');
+const tombol = document.querySelector('.dark');
+const t = dark.querySelector('.t')
 dark.addEventListener('click', function(){
-  style.setAttribute('href','css/dark.css')
+   link.setAttribute('href', 'css/dark.css');
+   const light = document.createElement('i');
+   const text = document.createTextNode('light_mode');
+   light.classList.add('material-icons');
+   light.appendChild(text);
+   dark.appendChild(light);
+   tombol.remove(dark);
+   dark.insertBefore(light,t);
 })
-// navigation
-ic.classList.replace('material-symbols-rounded','material-icons')
-nb.addEventListener('click', function(e) {
-  icon.forEach(function(icon) {
-    if ( icon.classList.contains('active') ){
-      icon.classList.remove('active');
-    }
-    if ( icon.classList.contains('material-icons') ){
-    icon.classList.replace('material-icons', 'material-symbols-rounded')
-    }
-  });
-  e.target.classList.add('active');
-  e.target.classList.replace('material-symbols-rounded', 'material-icons')
-})
-function submitData() {
-  const nilai = document.getElementById('name').value;
-  alert(nilai);
-}
+ic.classList.replace('material-symbols-rounded', 'material-icons');
+ico.classList.replace('material-symbols-rounded', 'material-icons');
+icon.forEach(function(pil){
+pil.addEventListener('click', function(e) {
+   icon.forEach(function(icon) {
+      if (icon.classList.contains('active')) {
+         icon.classList.remove('active');
+      }
+      if (icon.classList.contains('material-icons')) {
+         icon.classList.replace('material-icons', 'material-symbols-rounded')
+      }
+   });
+   e.target.classList.add('active');
+   e.target.classList.replace('material-symbols-rounded', 'material-icons')
+})})
